f64a64520f9fc8c5e85f0d606d341842e30f24bc
jack <lizhiqiangzixing@163.com>
2022-10-14 22:51:08 +0800

    you are a good boy

--- MyArrayTest/src/com/test/array/Demo1Array.java
+++ MyArrayTest/src/com/test/array/Demo1Array.java
@@ -1,6 +1,8 @@
 package com.test.array;
 
-public: class Demo1Array {
+import java.util.ArrayList;
+
+public class Demo1Array {
     public static void main(String[] args) {
         student[] arr1 = new student[3];
         System.out.println(arr1.length);
@@ -18,29 +20,29 @@ public: class Demo1Array {
             System.out.println(temp.getName()+"..."+temp.getAge());
         }
         student[] arr = new student[3];
-//        student stu1 = new student("zhangshan", 12);
-//        student stu2 = new student("lisi", 19);
-//        student stu3 = new student("wangwu", 21);
-//        System.out.println(stu1.getName() + "..." + stu1.getAge());
-//        System.out.println(stu2.getName() + "..." + stu2.getAge());
-//        System.out.println(stu3.getName() + "..." + stu3.getAge());
-//        ArrayList<String> list = new ArrayList<>();
-//        list.add("abc");
-//        list.add("123");
-//        list.add("helloworld");
-//        list.add(2,"11111");
+        student stu1 = new student("zhangshan", 12);
+        student stu2 = new student("lisi", 19);
+        student stu3 = new student("wangwu", 21);
+        System.out.println(stu1.getName() + "..." + stu1.getAge());
+        System.out.println(stu2.getName() + "..." + stu2.getAge());
+        System.out.println(stu3.getName() + "..." + stu3.getAge());
+        ArrayList<String> list = new ArrayList<>();
+        list.add("abc");
+        list.add("123");
+        list.add("helloworld");
+        list.add(2,"11111");
 //        list.add(123);
 //        list.add(true);
-//        System.out.println(list);
+        System.out.println(list);
 
-//        arr[0] = stu1;
-//        arr[1] = stu2;
-//        arr[2] = stu3;
-//        for (int i = 0; i < arr.length; i++) {
-//            student temp = arr[i];
-//            System.out.println(temp.getName() + "..." + temp.getAge());
-//
-//        }
+        arr[0] = stu1;
+        arr[1] = stu2;
+        arr[2] = stu3;
+        for (int i = 0; i < arr.length; i++) {
+            student temp = arr[i];
+            System.out.println(temp.getName() + "..." + temp.getAge());
+
+        }
 
     }
 
