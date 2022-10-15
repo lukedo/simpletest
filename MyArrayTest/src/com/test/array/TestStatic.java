1ad1d88270cd8207bcd81b8e6cdec67cb56fd0d8
jack <lizhiqiangzixing@163.com>
2022-10-15 09:50:13 +0800

    test static method

--- MyArrayTest/src/com/test/array/TestStatic.java
+++
@@ -1,5 +0,0 @@
-public class TestStatic {
-    public static void main(String[] args) {
-        System.out.println("hello static method!");
-    }
-}
---
+++ src/main/java/com/example/demo/hello/TestStatic.java
@@ -0,0 +1,28 @@
+package com.example.demo.hello;
+
+public class TestStatic {
+
+    public static class animal {
+        private String name;
+        private static int age;
+
+        public static void move() {
+            System.out.println("just move and do sports!");
+        }
+
+        public void fly() {
+            System.out.println("just try to fly!" + age);
+        }
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println("hello static method!");
+        animal dog = new animal();
+        dog.age = 12;
+        dog.name = "ok";
+        dog.fly();
+        dog.move();
+
+    }
+}
